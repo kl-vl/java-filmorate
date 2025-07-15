a270cba65c90d18165c7fa895afcd6d2ee6f8d64
kl-vl <191594458+kl-vl@users.noreply.github.com>
2025-07-16 02:47:02 +0300

    checkstyle. Director unused import

--- src/main/java/ru/yandex/practicum/filmorate/repository/DbDirectorRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/DbDirectorRepository.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.repository;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
